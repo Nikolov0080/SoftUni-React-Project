839ffd20da7829100bfb21372a5b91b1154fd7ac
Nikolov0080 <apofis12343@gmail.com>
2020-07-24 18:33:23 +0300

    changed: productCard-resizing

--- beehive/src/components/honeyCard/honeyCard.js
+++ beehive/src/components/honeyCard/honeyCard.js
@@ -7,9 +7,9 @@ const HoneyCard = (props) => {
     
     return (
         <div className={style.card}>
-            <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src={props.imageUrl} />
-                <Card.Body>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body style={{"text-align":"center"}}>
+                <Card.Img style={{height:"12em",width:"10em"}} variant="bottom" src={props.imageUrl} />
                     <Card.Title>{props.name}</Card.Title>
                     <Card.Text>
                         {props.description}
