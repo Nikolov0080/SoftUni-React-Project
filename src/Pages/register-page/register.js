3ba50547f350878b89f90b9401dc9e7283a126e3
Nikolov0080 <apofis12343@gmail.com>
2020-08-03 22:24:42 +0300

    changed: registerPage - migrated with hooks

--- src/Pages/register-page/register.js
+++ src/Pages/register-page/register.js
@@ -1,103 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PageLayout from '../../components/pageLayout/pageLayout';
 import Input from '../../components/input/input';
 import style from './register.module.css';
 import { Button } from 'react-bootstrap';
 import auth from '../../fire/fireAuth'
 
-class RegisterPage extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            email: '',
-            password: '',
-        }
 
-        this.setEmail = this.setEmail.bind(this);
-        this.setPassword = this.setPassword.bind(this);
-        this.setRePassword = this.setRePassword.bind(this);
-        this.registerUser = this.registerUser.bind(this);
-    }
+const RegisterPage = (props) => {
 
-    setEmail(email) {
-        this.setState({ email })
-    }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
 
-    setPassword(password) {
-        this.setState({ password })
-    }
 
-    setRePassword(rePassword) {
-        this.setState({ rePassword })
-    }
-
-    registerUser = (event) => {
+  const  registerUser = (event) => {
         event.preventDefault();
-
-        const {
-            email,
-            password,
-            rePassword
-        } = this.state
 
         if (password === rePassword) {
             auth.register(email, password);
-            this.props.history.push('/products')
+            props.history.push('/products')
 
         } else {
 
-            this.props.history.push('/register')
+            props.history.push('/register')
 
         }
     }
 
 
-    render() {
-        return (
-            <PageLayout title="Register">
-                <div className={style.register}>
-                    <form>
+    return (
+        <PageLayout title="Register">
+            <div className={style.register}>
+                <form>
 
 
-                        <Input
-                            name="email"
-                            type="email"
-                            id="email1"
-                            label="Email address"
-                            placeholder="Email"
-                            onChange={this.setEmail}
-                        />
+                    <Input
+                        name="email"
+                        type="email"
+                        id="email1"
+                        label="Email address"
+                        placeholder="Email"
+                        onChange={setEmail}
+                    />
 
-                        <Input
-                            name="password"
-                            type="password"
-                            id="pass"
-                            label="Password"
-                            placeholder="Enter password more than 6 symbols"
-                            onChange={this.setPassword}
-                        />
-
-
-                        <Input
-                            name="password"
-                            type="password"
-                            id="re-pass"
-                            label="Confirm password"
-                            placeholder="Same as password field"
-                            onChange={this.setRePassword}
-                        />
-
-                        <Button onClick={this.registerUser} type="submit" variant="primary">Register</Button>
+                    <Input
+                        name="password"
+                        type="password"
+                        id="pass"
+                        label="Password"
+                        placeholder="Enter password more than 6 symbols"
+                        onChange={setPassword}
+                    />
 
 
-                    </form>
+                    <Input
+                        name="password"
+                        type="password"
+                        id="re-pass"
+                        label="Confirm password"
+                        placeholder="Same as password field"
+                        onChange={setRePassword}
+                    />
+
+                    <Button onClick={registerUser} type="submit" variant="primary">Register</Button>
 
 
-                </div>
-            </PageLayout>
-        )
-    }
+                </form>
+
+
+            </div>
+        </PageLayout>
+    )
+
 }
+
+
+
 
 export default RegisterPage;
