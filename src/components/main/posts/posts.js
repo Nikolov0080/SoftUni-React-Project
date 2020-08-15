25f0f3a8065734354925a6693b3fe0c063b45356
Nikolov0080 <apofis12343@gmail.com>
2020-08-15 15:47:20 +0300

    created sorting on productPage

--- src/components/main/posts/posts.js
+++ src/components/main/posts/posts.js
@@ -1,26 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import HoneyCard from '../honeyCard/honeyCard';
-import { Row, Col, Button } from 'react-bootstrap'
+import { Row, Col, Button, Dropdown } from 'react-bootstrap'
 
 const Posts = ({ props }) => {
 
     const [items, setItems] = useState([]);
+    const [select, setSelect] = useState('Name A-Z');
 
     useEffect(() => {
         setItems(props);
-    }, [props,items]);
+    }, [props]);
 
-    const sort = () => {
-        setItems(items.sort((a, b) => { return a.price - b.price }))
-        console.log(items)
+    const sortByPriceDesc = () => {
+        const sorted = [...items].sort((a, b) => { return a.price - b.price });
+        setItems(sorted);
+        setSelect('Price - to +');
     }
 
+    const sortByPriceAsc = () => {
+        const sorted = [...items].sort((a, b) => { return b.price - a.price });
+        setItems(sorted);
+        setSelect('Price - to +');
+    }
+
+    const sortByName = () => {
+        const sorted = [...items].sort((val, b) => {
+            return val.name.localeCompare(b.name)
+        });
+        setItems(sorted)
+        setSelect('Name A-Z');
+    }
 
     return (
         <div>
             <div className="text-center container">
-                <Button onClick={sort} className="mr-5">Sort by Name</Button>
-                <Button>Sort by price</Button>
+                <Dropdown>
+Sort By
+                    <Dropdown.Toggle className="ml-3" variant="success" id="dropdown-basic">
+                        {select}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={sortByPriceDesc}>Price - to +</Dropdown.Item>
+                        <Dropdown.Item onClick={sortByPriceAsc}>Price + to -</Dropdown.Item>
+                        <Dropdown.Item onClick={sortByName}>Name A-Z</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
 
             <Row className="col-12" style={{ margin: "0 auto" }}>
